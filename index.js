61b8a8e0d6dd89e332e071b03280dcbed7023292
Abu-Sahad <abusahad603@gmail.com>
2023-06-10 11:59:26 +0600

    single data api create

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivpd0t4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,18 @@ async function run() {
         app.post('/addToy', async (req, res) => {
             const newAddToy = req.body;
             const result = await addToyCollection.insertOne(newAddToy)
+            res.send(result)
+        })
+
+        app.get('/addToy', async (req, res) => {
+            const cursor = addToyCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/addToy/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await addToyCollection.findOne(query)
             res.send(result)
         })
 
